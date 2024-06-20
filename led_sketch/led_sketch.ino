b404e32bceb947d504e19490fbde28030f76d9bc
Daniel Bergquist <daniel.bergquist@hey.com>
2024-06-20 14:19:14 -0600

    cancelable delay

--- led_sketch/led_sketch.ino
+++ led_sketch/led_sketch.ino
@@ -6,8 +6,8 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  while(!Serial) {
-    ; // Wait
+  while (!Serial) {
+    ;  // Wait
   }
 
   Serial.println("Connected");
@@ -15,19 +15,19 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  // if (currentMode != '1') {
-  //   return;
-  // }
+  if (currentMode != '1') {
+    return;
+  }
 
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   // Serial.println("On");
-  if (delayWhileInMode(3000, '1')) { 
+  if (delayWhileInMode(1000, '1')) {
     digitalWrite(LED_BUILTIN, LOW);
     return;
-  }                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  }                                // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
   // Serial.println("Off");
-  if (delayWhileInMode(3000, '1')) { return; }                      // wait for a second
+  if (delayWhileInMode(1000, '1')) { return; }  // wait for a second
 }
 
 // Creates a dealy while in a mode
@@ -35,9 +35,13 @@ void loop() {
 bool delayWhileInMode(unsigned long delayTime, char mode) {
 
   unsigned long remainingDelay = delayTime;
-  unsigned long checkInterval = 20;
+  unsigned long checkInterval = 100;
   do {
+    // If we are no longer in the mode, break out of this delay
     if (currentMode != mode) { return true; }
+
+    // The check interval is the greater of `remainingDelay` and `checkInterval`
+    // And the check interval is subtracted from the remaining delay
     if (remainingDelay < checkInterval) {
       checkInterval = remainingDelay;
       remainingDelay = 0;
@@ -46,24 +50,26 @@ bool delayWhileInMode(unsigned long delayTime, char mode) {
     }
 
     delay(checkInterval);
-  } while (remainingDelay == 0);
+  } while (remainingDelay != 0);
 
-  return false;
+  // If we the mod changed while in the last delay, we still need to return `true`
+  // otherwise, return false
+  return currentMode != mode;
 }
 
 void serialEvent() {
-    if (Serial.available()) {
-      // get the new byte:
-      char inputChar = (char)Serial.read();
-      if (inputChar == '\n') {  // don't care about newlines
-        return;
-      }
-      currentMode = inputChar;
-      Serial.println(currentMode);
-
-      // clear buffer (only interested in the first character)
-      while (Serial.available()) {
-        char devNull = (char)Serial.read();
-      }
+  if (Serial.available()) {
+    // get the new byte:
+    char inputChar = (char)Serial.read();
+    if (inputChar == '\n') {  // don't care about newlines
+      return;
     }
+    currentMode = inputChar;
+    Serial.println(currentMode);
+
+    // clear buffer (only interested in the first character)
+    while (Serial.available()) {
+      char devNull = (char)Serial.read();
+    }
+  }
 }
