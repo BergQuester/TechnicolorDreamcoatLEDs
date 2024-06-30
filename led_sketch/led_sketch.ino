0eaac49c954c95c9ce1ab48e6a8e6f576ef96aaf
Daniel Bergquist <daniel.bergquist@hey.com>
2024-06-29 20:30:37 -0600

    Pull in first FastLED scene

--- led_sketch/led_sketch.ino
+++ led_sketch/led_sketch.ino
@@ -1,10 +1,72 @@
+#include "FastLED.h"
+
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+// FastLED Config
+#define DATA_PIN    3
+#define TR_PIN      3
+#define TL_PIN      3
+#define MR_PIN      3
+#define ML_PIN      3
+#define BR_PIN      3
+#define BL_PIN      3
+//#define CLK_PIN   4
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    100
+#define TR_COUNT      3
+#define TL_COUNT      3
+#define MR_COUNT      3
+#define ML_COUNT      3
+#define BR_COUNT      3
+#define BL_COUNT      3
+#define BRIGHTNESS  255
+
+CRGB leds[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+CRGB leds3[NUM_LEDS];
+CRGB leds4[NUM_LEDS];
+CRGB leds5[NUM_LEDS];
+CRGB leds6[NUM_LEDS];
+
+// Which scene we are displaying
 char currentMode = '0';
 
 void setup() {
-  Serial.begin(9600);
 
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  delay(3000); // 3 second delay for recovery
+
+  Serial.begin(9600); // Set up serial coms
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,2,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.addLeds<LED_TYPE,3,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.addLeds<LED_TYPE,4,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.addLeds<LED_TYPE,5,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.addLeds<LED_TYPE,6,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.addLeds<LED_TYPE,7,COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip)
+    .setDither(BRIGHTNESS < 255);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
 
   while (!Serial) {
     ;  // Wait
@@ -15,19 +77,61 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  if (currentMode != '1') {
-    return;
+
+  switch (currentMode) {
+    case '1':
+      rainbow();
+      break;
+    default:
+
+      break;
   }
 
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  // Serial.println("On");
-  if (delayWhileInMode(1000, '1')) {
-    digitalWrite(LED_BUILTIN, LOW);
-    return;
-  }                                // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  // Serial.println("Off");
-  if (delayWhileInMode(1000, '1')) { return; }  // wait for a second
+  FastLED.show();
+}
+
+// Rainbow display
+// This function draws rainbows with an ever-changing,
+// widely-varying set of parameters.
+void rainbow() 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+    
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS-1) - pixelnumber;
+    
+    nblend( leds[pixelnumber], newcolor, 64);
+  }
 }
 
 // Creates a dealy while in a mode
@@ -57,6 +161,7 @@ bool delayWhileInMode(unsigned long delayTime, char mode) {
   return currentMode != mode;
 }
 
+// Get user input
 void serialEvent() {
   if (Serial.available()) {
     // get the new byte:
